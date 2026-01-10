63a4a01a642cbcf0047066f063f643b841e16f6e
rasseethali <raseethali46gmail.com>
2026-01-10 19:46:51 +0530

    updated backend

--- backend/routes/UserRoutes.js
+++ backend/routes/UserRoutes.js
@@ -1,7 +1,9 @@
 // routes/user.js or server.js
 import express from "express";
-import { signup } from "./controllers/auth.js"; // your signup function
+import { signup , login } from "./controllers/auth.js"; // your signup function
 const router = express.Router();
 
-router.post("/signup", signup);  // ✅ must be lowercase
+router.post("/signup", signup); 
+router.post("/login", login);
+// ✅ must be lowercase
 export default router;
